8e545168d6434bc5cb4626b8b3372ca051f186dc
Ethan Lipnik <ethanlipnik@gmail.com>
2021-09-01 20:51:25 -0400

    Created the basic website

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import logo from './logo.png';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  render() {
+    return (
+      <div className="p-10 space-y-10">
+        <header className="App-header container mx-auto">
+          <img src={logo} alt="Logo" width={300} height={300} className="mx-auto" />
+          <div className="mx-auto space-y-5">
+            <h1 className="text-center text-6xl font-semibold font-mono">OpenSesame</h1>
+            <h2 className="text-center text-3xl font-semibold font-mono opacity-70">Native, secure, open.</h2>
+            <div className="flex flex-col justify-center items-center text-center">
+              <a href="https://github.com/OpenSesameManager/OpenSesame" className="mx-auto bg-blue-500 w-1/3 p-4 rounded-xl shadow-xl font-bold text-gray-50 text-xl">View the source code</a>
+            </div>
+          </div>
+        </header>
+        <div className="space-y-5">
+          {this.detailView('What is it?', (
+            <p>OpenSesame is a free and powerful password manager that lets you manage your passwords with ease. It syncs with iCloud and encrypts your data to make sure only you get access. No more required subscriptions, no more ram hogs, and no more electron versions.</p>
+          ))}
+
+          {this.detailView('Why use OpenSesame?', (
+            <p>Now more than ever, what was previously amazing native apps have become slow and big web apps. Big apps take more control away from the user and push subscriptions and other payment models without much reason. OpenSesame aims to fix that by giving a free and open source password manager that is native and performant. Still get all the great features of other password managers without all the baggage.</p>
+          ))}
+
+          {this.detailView('Features', (
+            <ul className="list-disc list-inside">
+              <li>iCloud Syncing</li>
+              <li>OTP Auth (Two Factor Authentication)</li>
+              <li>Safari Autofill</li>
+              <li>On-Device AES encryption</li>
+              <li>Biometrics unlock</li>
+              <li>Pinned accounts</li>
+              <li>Multiple vaults</li>
+              <li>Multipeer account sharing</li>
+              <li>Import/Export</li>
+              <li>Password generator</li>
+              <li>Credit/Debit card support</li>
+              <li>Notes field</li>
+              <li>CoreData</li>
+              <li>100% SwiftUI</li>
+              <li>Native support for macOS and iOS.</li>
+            </ul>
+          ))}
+
+          {this.detailView('Planned Features', (
+            <ul className="list-disc list-inside">
+              <li>Better password bulk deleting</li>
+              <li>Menubar access</li>
+              <li>Dropbox, Google Drive, personal server support</li>
+              <li>OTP Autofill</li>
+              <li>OTP QR codes</li>
+              <li>Password suggestions (detect if password is secure)</li>
+              <li>Compromised/breached password notice</li>
+              <li>Chrome & native Windows support</li>
+            </ul>
+          ))}
+        </div>
+      </div>
+    );
+  }
+
+  detailView = (title, description) => {
+    return (
+      <div className="container mx-auto bg-gray-200 p-4 rounded-xl">
+        <h3 className="font-semibold text-xl">{title}</h3>
+        <p>{description}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
